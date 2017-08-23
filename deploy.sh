c3e02db6e1f88a64c631589ec0dc61414911a646
Aya Salah <aya.salah_93@outlook.com>
2017-08-23 13:52:39 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 if [[ $(kubectl get deployments | grep $DEPLOYMENT_NAME) ]]; then
 echo "deployments exists"
 else
-kubectl run $DEPLOYMENT_NAME --image=$IMAGE_NAME --replicas=$REPLICAS --output=yaml --dry-run > "$IMAGE_NAME_FILE-rc.yaml"
+kubectl run $DEPLOYMENT_NAME --image=$IMAGE_NAME --replicas=2 --output=yaml --dry-run > "$IMAGE_NAME_FILE-rc.yaml"
 kubectl create -f "$IMAGE_NAME_FILE-rc.yaml"
 fi
