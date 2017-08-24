b2890cd7d9a62604130604b8c29c73ac0bfaaf54
manar <manarahmedshehata@gmail.com>
2017-08-24 11:45:48 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-if [[ $(kubectl get deployments | grep andriostudio) ]]; then
+if [[ $(kubectl get deployments | grep $DEPLOYMENT_NAME) ]]; then
 echo "deployments exists"
 else
-kubectl run andriostudio --image=manar21/android-studio --replicas=2 --output=yaml --dry-run > "deployment-rc.yaml"
-kubectl create -f "deployment-rc.yaml"
+kubectl run andriostudio --image=$IMAGE_NAME --replicas=2 --output=yaml --dry-run > "$IMAGE_NAME-rc.yaml"
+kubectl create -f "$IMAGE_NAME.yaml"
 fi
