ac49ba7cc39f8d107eff84aef47157fd98ba3341
manar <manarahmedshehata@gmail.com>
2017-08-24 14:43:03 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ if [[ $(kubectl get deployments | grep $DEPLOYMENT_NAME) ]]; then
 echo "deployments exists"
 else
 echo "testttttttttttttttt" 
-kubectl run -i spainmobenv --image=docker.io/manar21/android-studio:junit-fabricplugin --replicas=1 --output=yaml > testspainmobenv-rc.yaml
-#kubectl run spainmobenv --image=docker.io/manar21/android-studio:junit-fabricplugin --command bash --replicas=2 --output=yaml --dry-run > spainmobenv-rc.yaml
-#kubectl create -f spainmobenv-rc.yaml
+#kubectl run -i spainmobenv --image=docker.io/manar21/android-studio:junit-fabricplugin --replicas=1 --output=yaml > testspainmobenv-rc.yaml
+kubectl run --tty spainmobenv --image=docker.io/manar21/android-studio:junit-fabricplugin --replicas=2 --output=yaml --dry-run > spainmobenv-rc-test.yaml
+kubectl create -f spainmobenv-rc-test.yaml
 fi
