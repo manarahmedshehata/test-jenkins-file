c921634b68701480ace30ebe63d53123aade6fba
manar <manarahmedshehata@gmail.com>
2017-08-24 15:36:41 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ else
 echo "testttttttttttttttt" 
 #kubectl run -i spainmobenv --image=docker.io/manar21/android-studio:junit-fabricplugin --replicas=1 --output=yaml > testspainmobenv-rc.yaml
 #kubectl run spainmobenv --image=docker.io/manar21/android-studio:junit-fabricplugin --replicas=2 --output=yaml --dry-run > spainmobenv-rc-test.yaml
-kubectl run spainmobenv --image=manar21/android-studio:junit-fabricplugin-up --replicas=2 --output=yaml --dry-run > spainmobenv-rc-test.yaml
+kubectl run spainmobenv --image=manar21/android-studio:junit-fabricplugin-up --port=5901 --replicas=2 --output=yaml --dry-run > spainmobenv-rc-test.yaml
 kubectl create -f spainmobenv-rc-test.yaml
 fi
