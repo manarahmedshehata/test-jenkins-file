9708d55f08a501f81ab86bfb5cf06343c8a8ce0f
manar <manarahmedshehata@gmail.com>
2017-08-24 11:49:00 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 if [[ $(kubectl get deployments | grep $DEPLOYMENT_NAME) ]]; then
 echo "deployments exists"
 else
-kubectl run andriostudio --image=$IMAGE_NAME --replicas=2 --output=yaml --dry-run > "$IMAGE_NAME-rc.yaml"
+kubectl run $DEPLOYMENT_NAME --image=$IMAGE_NAME --replicas=2 --output=yaml --dry-run > "$IMAGE_NAME-rc.yaml"
 kubectl create -f "$IMAGE_NAME.yaml"
 fi
