23722392bbb7455152ea7686e395118a40ed7a83
manar <manarahmedshehata@gmail.com>
2017-08-24 12:08:46 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@ if [[ $(kubectl get deployments | grep $DEPLOYMENT_NAME) ]]; then
 echo "deployments exists"
 else
 echo "testttttttttttttttt" 
-kubectl run $DEPLOYMENT_NAME --image=$IMAGE_NAME --replicas=2 --output=yaml --dry-run > $DEPLOYMENT_NAME-rc.yaml
+kubectl run spainmobileenv --image=$IMAGE_NAME --replicas=2 --output=yaml --dry-run > $DEPLOYMENT_NAME-rc.yaml
 kubectl create -f $DEPLOYMENT_NAME-rc.yaml
 fi
